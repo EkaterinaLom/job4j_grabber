a4f9a290e69724bb5b47be425f18efe79417468f
EkaterinaLom <Lomakinaei@mail.ru>
2023-04-08 21:20:22 +1000

    7. Web. [#289478]

--- src/main/java/ru/job4j/grabber/Grabber.java
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -4,6 +4,10 @@ import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.Charset;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Properties;
 import static org.quartz.JobBuilder.newJob;
@@ -12,20 +16,27 @@ import static org.quartz.TriggerBuilder.newTrigger;
 
 public class Grabber implements Grab {
 
-    private final Parse parse;
-    private final Store store;
-    private final Scheduler scheduler;
-    private final int time;
+    private final Properties cfg = new Properties();
 
-    public Grabber(Parse parse, Store store, Scheduler scheduler, int time) {
-        this.parse = parse;
-        this.store = store;
-        this.scheduler = scheduler;
-        this.time = time;
+    public Store store() throws SQLException {
+        return new PsqlStore(cfg);
+    }
+
+    public Scheduler scheduler() throws SchedulerException {
+        Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+        scheduler.start();
+        return scheduler;
+    }
+
+    public void cfg() throws IOException {
+        try (InputStream in = Grabber.class.getClassLoader()
+                .getResourceAsStream("post.properties")) {
+            cfg.load(in);
+        }
     }
 
     @Override
-    public void init() throws SchedulerException {
+    public void init(Parse parse, Store store, Scheduler scheduler) throws SchedulerException {
         JobDataMap data = new JobDataMap();
         data.put("store", store);
         data.put("parse", parse);
@@ -33,7 +44,7 @@ public class Grabber implements Grab {
                 .usingJobData(data)
                 .build();
         SimpleScheduleBuilder times = simpleSchedule()
-                .withIntervalInSeconds(time)
+                .withIntervalInSeconds(Integer.parseInt(cfg.getProperty("jdbc.time")))
                 .repeatForever();
         Trigger trigger = newTrigger()
                 .startNow()
@@ -61,17 +72,33 @@ public class Grabber implements Grab {
         }
     }
 
+    public void web(Store store) {
+        new Thread(() -> {
+            try (ServerSocket server = new ServerSocket(Integer.parseInt(cfg.getProperty("jdbc.port")))) {
+                while (!server.isClosed()) {
+                    Socket socket = server.accept();
+                    try (OutputStream out = socket.getOutputStream()) {
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        for (Post post : store.getAll()) {
+                            out.write(post.toString().getBytes(Charset.forName("Windows-1251")));
+                            out.write(System.lineSeparator().getBytes());
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
     public static void main(String[] args) throws Exception {
-        Properties conf = new Properties();
-        try (InputStream in = Grabber.class.getClassLoader()
-                .getResourceAsStream("post.properties")) {
-            conf.load(in);
-        }
-        Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
-        scheduler.start();
-        Parse parse = new HabrCareerParse(new HabrCareerDateTimeParser());
-        Store store = new PsqlStore(conf);
-        int time = Integer.parseInt(conf.getProperty("jdbc.time"));
-        new Grabber(parse, store, scheduler, time).init();
+        Grabber grabber = new Grabber();
+        grabber.cfg();
+        Scheduler scheduler = grabber.scheduler();
+        Store store = grabber.store();
+        grabber.init(new HabrCareerParse(new HabrCareerDateTimeParser()), store, scheduler);
+        grabber.web(store);
     }
 }
